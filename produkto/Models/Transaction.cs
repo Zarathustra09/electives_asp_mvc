4698deca3a8aa05ec8d07694d819979f77c354f1
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-03-07 22:36:42 +0800

    March 07 2024 - Users Crud

---
+++ produkto/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace produkto.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
